30ed934247dbb04f8fa0ea16a0614fa610033b96
vainius21 <vainius1232@gmail.com>
2020-12-08 19:39:50 +0200

    l

---
+++ UnitTestai/UnitTestai.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestai
+{
+	TEST_CLASS(UnitTestai)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
